6074c83817ae7c6192002ca023b8dfd3a82e85ef
dotero <diego.otero@mercadolibre.com>
2014-03-02 18:23:40 -0300

    create view to show all contributors

--- bfcm/app/controllers/Contributors.java
+++ bfcm/app/controllers/Contributors.java
@@ -1,11 +1,13 @@
 package controllers;
 
+import models.Contributor;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+
 public class Contributors extends Controller {
 	public static Result index() {
-		return TODO;
+		return ok(views.html.contributors.index.render(Contributor.get()));
 	}
 	
 	public static Result get(Long id) {
--- bfcm/app/models/Contributor.java
+++ bfcm/app/models/Contributor.java
@@ -1,9 +1,14 @@
 package models;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Contributor {
 	private Long	id;
 	private String 	name;
-	
+
 	public Contributor() {}
 
 	public Contributor(Long id, String name) {
@@ -14,17 +19,35 @@ public class Contributor {
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
+	}
+
+
+	private static Map<Long, Contributor>		contributors;
+
+	static {
+		contributors = new HashMap<Long, Contributor>();
+		contributors.put(1L, new Contributor(1L, "Diego"));
+		contributors.put(2L, new Contributor(2L, "Pablo"));
+		contributors.put(3L, new Contributor(3L, "Joel"));
+	}
+	
+	public static Set<Contributor> get() {
+		return new HashSet<Contributor>(contributors.values());
+	}
+	
+	public static Contributor get(Long id) {
+		return contributors.get(id);
 	}
 
 }
