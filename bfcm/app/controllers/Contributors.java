a97bc68d095c7eec2b373a789553293367b7d770
dotero <diego.otero@mercadolibre.com>
2014-03-02 16:42:41 -0300

    create Contributor controller and routes

---
+++ bfcm/app/controllers/Contributors.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Contributors extends Controller {
+	public static Result index() {
+		return TODO;
+	}
+	
+	public static Result get(Long id) {
+		return TODO;
+	}
+	
+	public static Result create() {
+		return TODO;
+	}
+	
+	public static Result save() {
+		return TODO;
+	}
+}
